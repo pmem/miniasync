b4de5e4b606f33beb66a9727b0d35376425f4b46
Krzysztof Swiecicki <krzysztof.swiecicki@intel.com>
2021-11-29 06:30:49 -0500

    masync: add dml mover implementation

---
+++ extras/dml/include/libminiasync-dml/vdm_dml.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright 2021, Intel Corporation */
+
+#include <dml/dml.h>
+#include <libminiasync.h>
+
+struct vdm_descriptor *vdm_descriptor_dml(void);
+struct vdm_descriptor *vdm_descriptor_dml_async(void);
