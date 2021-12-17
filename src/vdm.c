ae63af57da1ec73cd404abf121d1e692b32d7b0e
Piotr Balcer <piotr.balcer@intel.com>
2021-12-17 11:55:48 +0100

    masync: add expanded future notifier types
    This enables futures and runtimes to support a wider
    variety of different notification mechanisms for indicating
    that a future is ready to be polled again.
    This PR implements pollers, which can allow runtimes to use
    optimized umwait/umonitor instructions when possible.

--- src/vdm.c
+++ src/vdm.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-/* Copyright 2019-2021, Intel Corporation */
+/* Copyright 2021, Intel Corporation */
 
 #include <pthread.h>
 #include <stdlib.h>
@@ -48,17 +48,27 @@ vdm_memcpy_cb(struct future_context *context)
 {
 	struct vdm_memcpy_data *data = future_context_get_data(context);
 	util_atomic_store64(&data->complete, 1);
-	FUTURE_WAKER_WAKE(&data->waker);
+	if (data->notifier.notifier_used == FUTURE_NOTIFIER_WAKER)
+		FUTURE_WAKER_WAKE(&data->notifier.waker);
 }
 
 static enum future_state
-vdm_memcpy_impl(struct future_context *context, struct future_waker waker)
+vdm_memcpy_impl(struct future_context *context, struct future_notifier *n)
 {
 	struct vdm_memcpy_data *data = future_context_get_data(context);
 	if (context->state == FUTURE_STATE_IDLE) {
-		data->waker = waker;
+		if (n) {
+			data->notifier = *n;
+		} else {
+			data->notifier.notifier_used = FUTURE_NOTIFIER_NONE;
+		}
+
 		data->vdm_cb = vdm_memcpy_cb;
-		data->vdm->descriptor->memcpy(data->vdm->descriptor, context);
+		data->vdm->descriptor->memcpy(data->vdm->descriptor,
+			&data->notifier, context);
+		if (n) {
+			*n = data->notifier;
+		}
 	}
 	int complete;
 	util_atomic_load64(&data->complete, &complete);
@@ -81,12 +91,21 @@ vdm_memcpy(struct vdm *vdm, void *dest, void *src, size_t n)
 }
 
 static void
-memcpy_sync(void *runner, struct future_context *context)
+memcpy_impl(void *descriptor, struct future_context *context)
 {
 	struct vdm_memcpy_data *data = future_context_get_data(context);
 	struct vdm_memcpy_output *output = future_context_get_output(context);
 	output->dest = memcpy(data->dest, data->src, data->n);
 	data->vdm_cb(context);
+}
+
+static void
+memcpy_sync(void *descriptor, struct future_notifier *notifier,
+	struct future_context *context)
+{
+	notifier->notifier_used = FUTURE_NOTIFIER_NONE;
+
+	memcpy_impl(descriptor, context);
 }
 
 static struct vdm_descriptor synchronous_descriptor = {
@@ -104,14 +123,30 @@ vdm_descriptor_synchronous(void)
 static void *
 async_memcpy_pthread(void *arg)
 {
-	memcpy_sync(NULL, arg);
+	memcpy_impl(NULL, arg);
 
 	return NULL;
 }
 
 static void
-memcpy_pthreads(void *runner, struct future_context *context)
+memcpy_pthreads(void *descriptor, struct future_notifier *notifier,
+	struct future_context *context)
 {
+	notifier->notifier_used = FUTURE_NOTIFIER_WAKER;
+
+	pthread_t thread;
+	pthread_create(&thread, NULL, async_memcpy_pthread, context);
+}
+
+static void
+memcpy_pthreads_polled(void *descriptor, struct future_notifier *notifier,
+	struct future_context *context)
+{
+	struct vdm_memcpy_data *data = future_context_get_data(context);
+
+	notifier->notifier_used = FUTURE_NOTIFIER_POLLER;
+	notifier->poller.ptr_to_monitor = (uint64_t *)&data->complete;
+
 	pthread_t thread;
 	pthread_create(&thread, NULL, async_memcpy_pthread, context);
 }
@@ -126,4 +161,15 @@ struct vdm_descriptor *
 vdm_descriptor_pthreads(void)
 {
 	return &pthreads_descriptor;
+}
+
+static struct vdm_descriptor pthreads_polled_descriptor = {
+	.vdm_data = NULL,
+	.memcpy = memcpy_pthreads_polled,
+};
+
+struct vdm_descriptor *
+vdm_descriptor_pthreads_polled(void)
+{
+	return &pthreads_polled_descriptor;
 }
