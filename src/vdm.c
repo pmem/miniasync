1b6693607e065169fbc4e32bcc6de1ad08787bf2
blazej-smorawski <blazej.smorawski@gmail.com>
2021-12-31 09:36:00 +0100

    masync: threads memcpy with ringbuffer

--- src/vdm.c
+++ src/vdm.c
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /* Copyright 2021, Intel Corporation */
 
-#include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "libminiasync/vdm.h"
 #include "core/util.h"
 
@@ -13,15 +11,26 @@ struct vdm {
 	void *data;
 };
 
+/*
+ * vdm_new -- returns NULL if failed to allocate memory for struct vdm
+ * or vdm_data_init failed
+ */
 struct vdm *
 vdm_new(struct vdm_descriptor *descriptor)
 {
 	struct vdm *vdm = malloc(sizeof(struct vdm));
+	if (vdm == NULL || descriptor == NULL)
+		return NULL;
+
 	vdm->descriptor = descriptor;
 	vdm->data = NULL;
 
-	if (descriptor->vdm_data_init)
-		descriptor->vdm_data_init(&vdm->data);
+	if (descriptor->vdm_data_init) {
+		if (descriptor->vdm_data_init(&vdm->data) != 0) {
+			free(vdm);
+			return NULL;
+		}
+	}
 
 	return vdm;
 }
@@ -47,9 +56,18 @@ static void
 vdm_memcpy_cb(struct future_context *context)
 {
 	struct vdm_memcpy_data *data = future_context_get_data(context);
-	util_atomic_store64(&data->complete, 1);
 	if (data->notifier.notifier_used == FUTURE_NOTIFIER_WAKER)
 		FUTURE_WAKER_WAKE(&data->notifier.waker);
+	/*
+	 * Setting the complete flag has to be done after calling
+	 * the notifier because if the flag is set and runtime
+	 * spins just after that, it is possible that the runtime
+	 * will finish its work (because all complete flags
+	 * will be set) and when the worker thread calls
+	 * the notifier it will refer to main thread's stack address
+	 * that is no longer relevant and cause segmentation fault.
+	 */
+	util_atomic_store32(&data->complete, 1);
 }
 
 static enum future_state
@@ -82,31 +100,36 @@ vdm_memcpy(struct vdm *vdm, void *dest, void *src, size_t n, uint64_t flags)
 	future.data.dest = dest;
 	future.data.src = src;
 	future.data.n = n;
+	future.data.started = 0;
 	future.data.complete = 0;
-	future.output = (struct vdm_memcpy_output){ NULL };
+	future.output = (struct vdm_memcpy_output){NULL};
 	future.data.flags = flags;
 	FUTURE_INIT(&future, vdm_memcpy_impl);
-
 	return future;
 }
 
-static void
-memcpy_impl(void *descriptor, struct future_context *context)
-{
-	struct vdm_memcpy_data *data = future_context_get_data(context);
-	struct vdm_memcpy_output *output = future_context_get_output(context);
-	output->dest = memcpy(data->dest, data->src, data->n);
-	data->vdm_cb(context);
-}
-
-static enum future_state
+enum future_state
 vdm_check(struct future_context *context)
 {
 	struct vdm_memcpy_data *data = future_context_get_data(context);
 
 	int complete;
-	util_atomic_load64(&data->complete, &complete);
+	util_atomic_load32(&data->complete, &complete);
 	return (complete) ? FUTURE_STATE_COMPLETE : FUTURE_STATE_RUNNING;
+}
+
+enum future_state
+vdm_check_async_start(struct future_context *context)
+{
+	struct vdm_memcpy_data *data = future_context_get_data(context);
+	int started, complete;
+	util_atomic_load32(&data->started, &started);
+	util_atomic_load32(&data->complete, &complete);
+	if (complete)
+		return FUTURE_STATE_COMPLETE;
+	if (started)
+		return FUTURE_STATE_RUNNING;
+	return FUTURE_STATE_IDLE;
 }
 
 static void
@@ -115,7 +138,10 @@ memcpy_sync(void *descriptor, struct future_notifier *notifier,
 {
 	notifier->notifier_used = FUTURE_NOTIFIER_NONE;
 
-	memcpy_impl(descriptor, context);
+	struct vdm_memcpy_data *data = future_context_get_data(context);
+	struct vdm_memcpy_output *output = future_context_get_output(context);
+	output->dest = memcpy(data->dest, data->src, data->n);
+	util_atomic_store32(&data->complete, 1);
 }
 
 static struct vdm_descriptor synchronous_descriptor = {
@@ -129,61 +155,4 @@ struct vdm_descriptor *
 vdm_descriptor_synchronous(void)
 {
 	return &synchronous_descriptor;
-}
-
-static void *
-async_memcpy_pthread(void *arg)
-{
-	memcpy_impl(NULL, arg);
-
-	return NULL;
-}
-
-static void
-memcpy_pthreads(void *descriptor, struct future_notifier *notifier,
-	struct future_context *context)
-{
-	notifier->notifier_used = FUTURE_NOTIFIER_WAKER;
-
-	pthread_t thread;
-	pthread_create(&thread, NULL, async_memcpy_pthread, context);
-}
-
-static void
-memcpy_pthreads_polled(void *descriptor, struct future_notifier *notifier,
-	struct future_context *context)
-{
-	struct vdm_memcpy_data *data = future_context_get_data(context);
-
-	notifier->notifier_used = FUTURE_NOTIFIER_POLLER;
-	notifier->poller.ptr_to_monitor = (uint64_t *)&data->complete;
-
-	pthread_t thread;
-	pthread_create(&thread, NULL, async_memcpy_pthread, context);
-}
-
-static struct vdm_descriptor pthreads_descriptor = {
-	.memcpy = memcpy_pthreads,
-	.vdm_data_init = NULL,
-	.vdm_data_fini = NULL,
-	.check = vdm_check,
-};
-
-struct vdm_descriptor *
-vdm_descriptor_pthreads(void)
-{
-	return &pthreads_descriptor;
-}
-
-static struct vdm_descriptor pthreads_polled_descriptor = {
-	.memcpy = memcpy_pthreads_polled,
-	.vdm_data_init = NULL,
-	.vdm_data_fini = NULL,
-	.check = vdm_check,
-};
-
-struct vdm_descriptor *
-vdm_descriptor_pthreads_polled(void)
-{
-	return &pthreads_polled_descriptor;
 }
