378833f061f9bbe59c2699645f230db407036bf3
Krzysztof Swiecicki <krzysztof.swiecicki@intel.com>
2022-01-25 04:41:43 -0500

    masync: add movdir64b instruction check
    Valgrind reports illegal instruction when running tests with linked
    DML that was compiled with HW option. Valgrind most probably
    doesn't recognise the movdir64b instruction.

--- tests/dml_vdm/test_dml_vdm.cmake
+++ tests/dml_vdm/test_dml_vdm.cmake
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2021, Intel Corporation
+# Copyright 2021-2022, Intel Corporation
 
 # an example for the dml_vdm test case
 
@@ -7,6 +7,13 @@ include(${SRC_DIR}/cmake/test_helpers.cmake)
 
 setup()
 
-execute(0 ${TEST_DIR}/dml_vdm)
+# check for MOVDIR64B instruction
+execute_process(COMMAND ${CMAKE_CURRENT_BINARY_DIR}/../extras/dml/check_movdir64b
+			RESULT_VARIABLE RET)
+
+# execute DML tests only if MOVDIR64B instruction is available
+if (RET EQUAL 0)
+	execute(0 ${TEST_DIR}/dml_vdm)
+endif()
 
 cleanup()
