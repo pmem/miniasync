de1f868491a0dded00ac34a8482ab8c866a4eef7
Piotr Balcer <piotr.balcer@intel.com>
2022-03-10 15:16:23 +0100

    masync: make vdm operation accessible in the future
    Right now the vdm_operation_future hides all operation
    details behind a mover-specific data pointer. This
    makes it impossible to alter the operation arguments
    inside of a chained future. This patch moves all operation
    data to the the externally visible vdm_operation_data.

--- tests/data_mover_dml/data_mover_dml.c
+++ tests/data_mover_dml/data_mover_dml.c
@@ -8,11 +8,11 @@
 
 #include <libminiasync.h>
 #include <libminiasync-vdm-dml.h>
-
+#include "libminiasync-vdm-dml/data_mover_dml.h"
 #include "util_dml.h"
 
 static int
-dml_memcpy(unsigned flags)
+dml_memcpy(enum data_mover_dml_type type, uint64_t flags)
 {
 	char *buf_a = strdup("testbuf");
 	char *buf_b = strdup("otherbuf");
@@ -20,7 +20,7 @@ dml_memcpy(unsigned flags)
 
 	struct runtime *r = runtime_new();
 
-	struct data_mover_dml *dmd = data_mover_dml_new();
+	struct data_mover_dml *dmd = data_mover_dml_new(type);
 	struct vdm *dml_mover_async = data_mover_dml_get_vdm(dmd);
 
 	struct vdm_operation_future a_to_b = vdm_memcpy(dml_mover_async, buf_b,
@@ -42,19 +42,19 @@ dml_memcpy(unsigned flags)
 static int
 test_dml_basic_memcpy()
 {
-	return dml_memcpy(0);
+	return dml_memcpy(DATA_MOVER_DML_SOFTWARE, 0);
 }
 
 static int
 test_dml_durable_flag_memcpy()
 {
-	return dml_memcpy(MINIASYNC_DML_F_MEM_DURABLE);
+	return dml_memcpy(DATA_MOVER_DML_SOFTWARE, MINIASYNC_DML_F_MEM_DURABLE);
 }
 
 static int
 test_dml_hw_path_flag_memcpy()
 {
-	return dml_memcpy(MINIASYNC_DML_F_PATH_HW);
+	return dml_memcpy(DATA_MOVER_DML_HARDWARE, 0);
 }
 
 int
