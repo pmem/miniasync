cb54bb851902b0453d333b1217593acc2e0f7813
blazej-smorawski <blazej.smorawski@gmail.com>
2022-03-23 09:28:00 +0100

    masync: add future_poll test and fix tests using dml

--- tests/vdm_operation_future_poll/test_vdm_operation_future_poll.cmake
+++ tests/vdm_operation_future_poll/test_vdm_operation_future_poll.cmake
@@ -1,7 +1,5 @@
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2021-2022, Intel Corporation
-
-# an example for the data_mover_dml test case
+# Copyright 2022, Intel Corporation
 
 include(${SRC_DIR}/cmake/test_helpers.cmake)
 
@@ -9,13 +7,13 @@ setup()
 
 # check for MOVDIR64B instruction
 execute_process(COMMAND ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${BUILD}/check_movdir64b
-		RESULT_VARIABLE RET)
+        RESULT_VARIABLE RET)
 
 # execute DML tests only if MOVDIR64B instruction is available
 if (RET EQUAL 0)
-	execute(0 ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${BUILD}/data_mover_dml)
+    execute(0 ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${BUILD}/vdm_operation_future_poll)
 else()
-	message("Wrong return from check_movdir64b: ${RET}")
+    message("Wrong return from check_movdir64b: ${RET}")
 endif()
 
 cleanup()
