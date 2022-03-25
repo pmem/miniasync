d8661fed9d0af9a8a105043ea14972df34e91368
damian <damian.duy@intel.com>
2022-03-25 12:39:26 +0100

    masync: add memmove operation to the thread data mover documentation

--- tests/memmove_threads/memmove_threads.c
+++ tests/memmove_threads/memmove_threads.c
@@ -8,14 +8,8 @@
 #include "core/os.h"
 #include "test_helpers.h"
 
-enum test_type {SEQUENCE, SINGLE_CHAR};
-/*
- * test_threads_memmove_multiple -- test multiple memmove operations n times
- * in a loop on a specific descriptor and test type.
- */
 int
-test_threads_memmove_multiple(unsigned memmove_count,
-	unsigned n, size_t test_size, enum test_type test_type)
+test_thread_memmove_single(size_t str_len)
 {
 	int ret = 0;
 	unsigned seed = (unsigned)time(NULL);
@@ -29,90 +23,119 @@ test_threads_memmove_multiple(unsigned memmove_count,
 
 	struct vdm *vdm = data_mover_threads_get_vdm(dmt);
 
-	char **sources = malloc(memmove_count * sizeof(char *) * n);
-	if (sources == NULL)
-		UT_FATAL("sources out of memory");
-	char **destinations = malloc(memmove_count * sizeof(char *) * n);
-	if (destinations == NULL)
-		UT_FATAL("destinations out of memory");
-	size_t *sizes = malloc(memmove_count * sizeof(size_t) * n);
-	if (sizes == NULL)
-		UT_FATAL("sizes out of memory");
+	char *buf_a = malloc(str_len + 1);
+	char *buf_b = malloc(str_len + 1);
 
-	struct future **futures = malloc(
-		memmove_count * sizeof(struct future *) * n);
-	if (futures == NULL)
-		UT_FATAL("futures out of memory");
-	struct vdm_operation_future *memmove_futures =
-		malloc(memmove_count * sizeof(struct vdm_operation_future) * n);
-	if (memmove_futures == NULL)
-		UT_FATAL("memmove_futures out of memory");
+	if (!buf_a || !buf_b)
+		UT_FATAL("buffers out of memory");
 
-	unsigned index = 0;
-	char value = 0;
-	for (unsigned iter = 0; iter < n; iter++) {
-		for (unsigned i = index; i < index + memmove_count; i++) {
-			if (test_size) {
-				sizes[i] = test_size;
-			} else {
-				sizes[i] = (size_t)os_rand_r(&seed)
-					% (1 << 20) + 1;
-			}
-			sources[i] = malloc(sizes[i] * sizeof(char));
-			destinations[i] = malloc(sizes[i] * sizeof(char));
-
-			switch (test_type) {
-				case SEQUENCE:
-					value = 0;
-					for (unsigned j = 0;
-						j < sizes[i]; j++) {
-						sources[i][j] = value++;
-					}
-					break;
-				case SINGLE_CHAR:
-					memset(sources[i], value++, sizes[i]);
-					break;
-				default:
-					break;
-			}
-
-			memmove_futures[i] = vdm_memmove(vdm, destinations[i],
-				sources[i], sizes[i], 0);
-			futures[i] =
-				FUTURE_AS_RUNNABLE(&memmove_futures[i]);
-		}
-
-		runtime_wait_multiple(r, futures + iter * memmove_count,
-			memmove_count);
-		index += memmove_count;
+	for (size_t i = 0; i < str_len; i++) {
+		buf_a[i] = (char)os_rand_r(&seed) % 256;
+		buf_b[i] = (char)os_rand_r(&seed) % 256;
 	}
 
-	/* Verification */
-	for (unsigned i = 0; i < memmove_count * n; i++) {
-		if (memcmp(sources[i], destinations[i], sizes[i]) != 0) {
-			fprintf(stderr,
-				"Memmove nr. %u result is wrong! "
-				"Returning\n", i);
-
-			ret = 1;
-			goto cleanup;
-		}
+	if (strcmp(buf_a, buf_b) == 0) {
+		UT_FATAL("buffers contain the same value before memmove");
 	}
 
-	/* Cleanup */
-cleanup:
-	for (unsigned i = 0; i < memmove_count * n; i++) {
-		free(sources[i]);
-		free(destinations[i]);
+	struct vdm_operation_future test_memmove_fut =
+		vdm_memmove(vdm, buf_a, buf_b, str_len, 0);
+
+	struct future *future_memmove = FUTURE_AS_RUNNABLE(&test_memmove_fut);
+
+	runtime_wait(r, future_memmove);
+
+	for (size_t i = 0; i < str_len; i++) {
+		UT_ASSERTeq(buf_a[i], buf_b[i]);
 	}
-	free(sources);
-	free(destinations);
-	free(sizes);
-	free(futures);
-	free(memmove_futures);
+
+	free(buf_a);
+	free(buf_b);
 
 	runtime_delete(r);
 	data_mover_threads_delete(dmt);
+
+	return ret;
+}
+
+int
+test_thread_memmove_multiple(size_t str_len)
+{
+	int ret = 0;
+	unsigned seed = (unsigned)time(NULL);
+	struct runtime *r = runtime_new();
+	struct data_mover_threads *dmt = data_mover_threads_default();
+
+	if (dmt == NULL) {
+		fprintf(stderr,
+				"error while creating synchronous data mover");
+		runtime_delete(r);
+		return 1;
+	}
+
+	struct vdm *vdm = data_mover_threads_get_vdm(dmt);
+
+	char *buf_a = malloc(str_len + 1);
+	char *buf_b = malloc(str_len + 1);
+	char *buf_c = malloc(str_len + 1);
+	char *buf_d = malloc(str_len + 1);
+
+	if (!buf_a || !buf_b || !buf_c || !buf_d)
+		UT_FATAL("buffers out of memory");
+
+	for (size_t i = 0; i < str_len; i++) {
+		buf_a[i] = (char)os_rand_r(&seed) % 256;
+		buf_b[i] = (char)os_rand_r(&seed) % 256;
+		buf_c[i] = (char)os_rand_r(&seed) % 256;
+		buf_d[i] = (char)os_rand_r(&seed) % 256;
+	}
+
+	struct future **futures = malloc(sizeof(struct future *) * 4);
+	if (futures == NULL)
+		UT_FATAL("futures out of memory");
+
+	struct vdm_operation_future memmove_fut_b_to_a =
+		vdm_memmove(vdm, buf_a, buf_b, str_len / 2, 0);
+
+	struct vdm_operation_future memmove_fut_c_to_a =
+		vdm_memmove(vdm, buf_a + str_len / 2, buf_c,
+			str_len / 2, 0);
+
+	struct vdm_operation_future memmove_fut_b_to_d =
+		vdm_memmove(vdm, buf_d, buf_b, str_len / 2, 0);
+
+	struct vdm_operation_future memmove_fut_c_to_d =
+		vdm_memmove(vdm, buf_d + str_len / 2, buf_c,
+			str_len / 2, 0);
+
+	futures[0] = FUTURE_AS_RUNNABLE(&memmove_fut_b_to_a);
+	futures[1] = FUTURE_AS_RUNNABLE(&memmove_fut_c_to_a);
+	futures[2] = FUTURE_AS_RUNNABLE(&memmove_fut_b_to_d);
+	futures[3] = FUTURE_AS_RUNNABLE(&memmove_fut_c_to_d);
+
+	runtime_wait_multiple(r, futures, 4);
+
+	for (size_t i = 0; i < str_len / 2; i++) {
+		UT_ASSERTeq(buf_a[i], buf_b[i]);
+		UT_ASSERTeq(buf_d[i], buf_b[i]);
+	}
+
+	size_t j = 0;
+	for (size_t i = str_len / 2; i < str_len; i++) {
+		UT_ASSERTeq(buf_a[i], buf_c[j]);
+		UT_ASSERTeq(buf_d[i], buf_c[j]);
+		j++;
+	}
+
+	free(buf_a);
+	free(buf_b);
+	free(buf_c);
+	free(buf_d);
+	free(futures);
+
+	runtime_delete(r);
+	data_mover_threads_delete(dmt);
+
 	return ret;
 }
 
@@ -120,10 +143,10 @@ int
 main(void)
 {
 	return
-		test_threads_memmove_multiple(100, 10, 10, SINGLE_CHAR) ||
-		test_threads_memmove_multiple(100, 2, 1 << 10, SINGLE_CHAR) ||
-		test_threads_memmove_multiple(100, 10, 128, SINGLE_CHAR) ||
-		test_threads_memmove_multiple(100, 10, 7, SEQUENCE) ||
-		test_threads_memmove_multiple(100, 1, 1 << 10, SEQUENCE) ||
-		test_threads_memmove_multiple(100, 10, 0, SEQUENCE);
+		test_thread_memmove_single(10000000) ||
+		test_thread_memmove_single(30000000) ||
+		test_thread_memmove_single(50000000) ||
+		test_thread_memmove_multiple(10000000) ||
+		test_thread_memmove_multiple(30000000) ||
+		test_thread_memmove_multiple(50000000);
 }
